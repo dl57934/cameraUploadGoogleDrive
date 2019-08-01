043c14043b39e26ec7e8100434e4f4a1a3508ba5
dl57934 <dl57934@pukyong.ac.kr>
2019-08-02 00:34:42 +0900

    add location spinner

--- server/index.py
+++ server/index.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import json
 from imageProcessing import getAverageRGB
 from saveCsv import saveAtCsv
+import os
 
 app = Flask(__name__)
 
@@ -14,16 +15,17 @@ def root():
 @app.route("/sendImage", methods=["GET", "POST"])
 def test():
     receive_file = request.files['image']
-    path = "/resource/" + receive_file.filename
+    location = request.args.get("location")
     receive_file.save(receive_file.filename)
 
     bgr = getAverageRGB(path=receive_file.filename)
-    data = {"imageName":receive_file.filename,
+    data = {"imageName": receive_file.filename,
             "blue": bgr[0], "green": bgr[1], "red": bgr[2],
-            "location": 1, "flowerShape": 0.8}
+            "location": location, "flowerShape": 0.8}
     saveAtCsv(new_data=data, path="dataSet.csv")
+    os.remove(receive_file.filename)
     return json.dumps({"success": "hihi"})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
